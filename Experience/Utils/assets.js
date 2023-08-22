4f4bca87fe458991b6a3f07ca5ad7da07c8eeba8
cho <hisstoryxx@gmail.com>
2023-08-22 20:23:12 +0900

    compress4

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -2,7 +2,7 @@ export default [
     {
         name: "room",
         type: "glbModel",
-        path: "/models/compress2.glb",
+        path: "/models/compress4.glb",
     },
     // {
     //     name: "screen",
